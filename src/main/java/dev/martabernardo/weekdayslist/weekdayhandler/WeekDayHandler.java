6aa974aa2f6ea083e5034ccb46de8752cef2cf65
MartaBernardoZamora <martabernardozamora@gmail.com>
2025-01-31 19:55:32 +0100

    Test createList method created and failed

---
+++ src/main/java/dev/martabernardo/weekdayslist/weekdayhandler/WeekDayHandler.java
@@ -0,0 +1,10 @@
+package dev.martabernardo.weekdayslist.weekdayhandler;
+
+public class WeekDayHandler {
+
+    public void createList() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createList'");
+    }
+
+}
---
+++ src/test/java/dev/martabernardo/weekdayslist/weekdayhandler/WeekDayHandlerTest.java
@@ -0,0 +1,21 @@
+package dev.martabernardo.weekdayslist.weekdayhandler;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+
+public class WeekDayHandlerTest {
+
+    @Test
+    @DisplayName("Test createList method")
+    void testCreateList() {
+        WeekDayHandler weekDayHandler = new WeekDayHandler();
+        weekDayHandler.createList();
+
+       assertThat(weekDayHandler, is(notNullValue()));
+
+    }
+}
